9ccdbb479d54dfd1c2fd1c6edb31cd52dbd8a878
theirAspiraions <>
2022-11-23 18:17:44 +0100

    "django framework changes"

--- website/image_tagger/views.py
+++ website/image_tagger/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
